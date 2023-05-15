06222d9fd510ac72427eac309fb14944366c7e58
Rustam Kurganov <kurganovrus@gmail.com>
2023-05-15 16:21:51 +0300

    upd

--- src/main/java/rus/kurganov/Main.java
+++ src/main/java/rus/kurganov/Main.java
@@ -1,8 +1,11 @@
 package rus.kurganov;
 
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello qa.guru!");
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
     }
+
+
 }
